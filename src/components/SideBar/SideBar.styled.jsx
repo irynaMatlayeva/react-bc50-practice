16fcf1f96180df5e3c67a4e494d24756d0ae24e2
oleniuh <maxim.oleniukh@gmail.com>
2023-04-25 13:56:47 +0100

    styled university card and sidebar

---
+++ src/components/SideBar/SideBar.styled.jsx
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+
+export const LogoStyled = styled.div`
+    background-color: #161D2A;
+    height: 80px;
+`
+
+export const InnerContainer = styled.div `
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+padding: 24px;
+height: 90vh;
+background-color: #FFFFFF;
+`
+
+export const UserContainer = styled.div `
+    display: flex;
+    gap: 10px;
+    align-items: center;
+`
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
-export {default as SideBar} from './SideBar/SideBar';
-export {default as Main} from './Main/Main'
-export {default as Paper} from './Paper/Paper'
+export { default as SideBar } from './SideBar/SideBar';
+export { default as Main } from './Main/Main';
+export { default as Paper } from './Paper/Paper';
+export { default as Card } from './Card/Card';
