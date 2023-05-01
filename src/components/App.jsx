b39a511468a7ab22ca11fa115e2d569fbe96e8f7
ViktorOsu <123021878+ViktorOsu@users.noreply.github.com>
2023-05-01 15:50:45 +0300

    added info forms and show Forms

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,9 +9,11 @@ import {
   PartialList,
   Button,
   TutorForm,
+  PartialForm,
 } from '../components';
 import universityData from '../constants/universityData.json';
 import tutorsIcon from '../assets/images/teachers-emoji.png';
+import FORMS from 'constants/forms';
 
 class App extends Component {
   state = {
@@ -19,19 +21,67 @@ class App extends Component {
     cities: universityData.cities.map(city => ({ text: city })) ?? [],
     departments:
       universityData.department.map(({ name }) => ({ text: name })) ?? [],
+    showForm: null,
   };
-  
+
   onEdit = () => console.log('Edit');
-  
+
   onDelete = () => console.log('Delete');
-  
+
   handleDropdown = () => console.log('dropdown');
 
-  addTutor = (tutor) => {
-    this.setState(({tutors}) => ({ tutors: [...tutors, tutor] }));
-  }
+  addTutor = tutor => {
+    this.setState(({ tutors }) => ({
+      tutors: [...tutors, tutor],
+      showForm: null,
+    }));
+  };
+
+  deleteTutor = name => {
+    this.setState(({ tutors }) => ({
+      tutors: tutors.filter(tutor => tutor.firstName !== name),
+    }));
+  };
+
+  addCity = name => {
+    if (
+      this.state.cities.some(
+        city => city.text.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`This City exist`);
+    } else {
+      this.setState(prevState => ({
+        cities: [...prevState.cities, { text: name }],
+        showForm: null,
+      }));
+    }
+  };
+
+  addDepartment = name => {
+    if (
+      this.state.departments.some(
+        department => department.text.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`This Department exist`);
+    } else {
+      this.setState(prevState => ({
+        departments: [...prevState.departments, { text: name }],
+        showForm: null,
+      }));
+    }
+  };
+
+  handleShowForm = name => {
+    this.setState(prevState => ({
+      showForm: prevState.showForm === name ? null : name,
+    }));
+  };
 
   render() {
+    console.log(this.state.showForm);
+
     return (
       <div className="app">
         <SideBar />
@@ -45,23 +95,49 @@ class App extends Component {
             <Paper>{universityData.description}</Paper>
           </Section>
           <Section title="Tutors" image={tutorsIcon}>
-            <TutorsList tutors={this.state.tutors} />
-            <TutorForm addTutor={this.addTutor} />
-            <Button isIcon text={'Add tutor'} />
+            <TutorsList
+              tutors={this.state.tutors}
+              deleteTutor={this.deleteTutor}
+            />
+            {this.state.showForm === FORMS.TUTOR_FORM && (
+              <TutorForm addTutor={this.addTutor} />
+            )}
+            <Button
+              isIcon
+              text={'Add tutor'}
+              action={() => this.handleShowForm(FORMS.TUTOR_FORM)}
+            />
           </Section>
           <Section title="Cities">
             <PartialList
               listData={this.state.cities}
               isOpenDropdown={this.handleDropdown}
             />
-            <Button isIcon text={'Add city'} />
+            {this.state.showForm === FORMS.CITY_FORM && (
+              <PartialForm title="adding city" onSubmit={this.addCity} />
+            )}
+            <Button
+              isIcon
+              text={'Add city'}
+              action={() => this.handleShowForm(FORMS.CITY_FORM)}
+            />
           </Section>
           <Section title="Departments">
             <PartialList
               listData={this.state.departments}
               isOpenDropdown={this.handleDropdown}
             />
-            <Button isIcon text={'Add department'} />
+            {this.state.showForm === FORMS.DEPARTMENT_FORM && (
+              <PartialForm
+                title="adding department"
+                onSubmit={this.addDepartment}
+              />
+            )}
+            <Button
+              isIcon
+              text={'Add department'}
+              action={() => this.handleShowForm(FORMS.DEPARTMENT_FORM)}
+            />
           </Section>
         </Main>
       </div>
---
+++ src/constants/forms.js
@@ -0,0 +1,6 @@
+const TUTOR_FORM = 'tutorForm';
+const CITY_FORM = 'cityForm';
+const DEPARTMENT_FORM = 'departmentForm';
+const FORMS = { TUTOR_FORM, CITY_FORM, DEPARTMENT_FORM };
+
+export default FORMS;
